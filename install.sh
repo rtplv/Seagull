6023d263ecc73268785d92d2d5edb3e35159eb1e
Ruslan Tupolev <rtplv@yandex.ru>
2020-12-03 18:35:48 +0300

    Fix conf

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+bash
 python3 -m venv ./venv
 ./venv/bin/pip install -r requirements.txt
+mkdir ./logs
 cp ./seagull-serv.conf /etc/supervisor/conf.d/seagull-serv.conf
 supervisorctl update all
